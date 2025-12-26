b18752c13253f4246f4372a814e7f662d7d5cfcd
Anuj Dwivedi <anujdwivedi0705@gmail.com>
2025-12-26 12:24:26 +0530

    Play with route

--- Chapter-S02-04-Routing-And-Request-Handler/app.js
+++ Chapter-S02-04-Routing-And-Request-Handler/app.js
@@ -8,13 +8,40 @@ const PORT = 3000;
 // });
 
 // Specific route for GET /user
-app.get("/user", (req, res) => {
-  res.send({ firstName: "John", lastName: "Doe" });
-});
+// app.get("/user", (req, res) => {
+//   res.send({ firstName: "John", lastName: "Doe" });
+// });
 
 // General route for all other /user requests
-app.post("/user", (req, res) => {
-  res.send("User created successfully!");
+// app.post("/user", (req, res) => {
+//   res.send("User created successfully!");
+// });
+
+// Playing with route parameters
+app.get("/user/:userId/:userName/:userAge", (req, res) => {
+  const userId = req.params.userId;
+  const userName = req.params.userName;
+  const userAge = req.params.userAge;
+  res.send(
+    `User ID requested: ${userId}, User Name requested: ${userName}, User Age requested: ${userAge}`
+  );
+});
+
+// Playing with "+, *, ?, () operators in route parameters"
+app.get("/abc?d", (req, res) => {
+  res.send("Matched route with optional 'c'");
+});
+
+app.get("/ab+c", (req, res) => {
+  res.send("Matched route with one or more 'b's");
+});
+
+app.get("/ab*c", (req, res) => {
+  res.send("Matched route with zero or more 'b's");
+});
+
+app.get("/ab(cd)?e", (req, res) => {
+  res.send("Matched route with optional 'cd'");
 });
 
 app.listen(PORT, () => {
