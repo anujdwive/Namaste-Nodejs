45bc01932cf844f500f838ba746d50cebbefac3c
Anuj Dwivedi <anujdwivedi0705@gmail.com>
2025-12-30 17:20:08 +0530

    add data into database

--- Chapter-S02-06-Database&Schema&Mongoose/app.js
+++ Chapter-S02-06-Database&Schema&Mongoose/app.js
@@ -1,7 +1,21 @@
 const expess = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = expess();
 const PORT = 3000;
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Anuj",
+    lastName: "Dwivedi",
+    email: "anujtest@gmail.com",
+    password: "Anuj@124",
+    gender: "male",
+  });
+
+  await user.save();
+  res.send("User added successfully...");
+});
 
 connectDB()
   .then(() => {
